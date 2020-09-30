502a7af1b1cf1816bac7c82387e0173fe705c769
yclimb <yclimb@qq.com>
2020-09-30 17:25:00 +0800

    服务消费者使用 @LoadBalanced RestTemplate 实现服务调用

---
+++ nacos-discovery-consumer-sample/src/main/java/com/example/demo/RestTemplateController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 服务调用
+ *
+ * @author yclimb
+ * @date 2020/9/30
+ */
+@RestController
+public class RestTemplateController {
+
+    @LoadBalanced
+    @Autowired
+    public RestTemplate restTemplate;
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/call/echo/{message}")
+    public String callEcho(@PathVariable String message) {
+        // 访问应用 nacos-discovery-provider-sample 的 REST "/echo/{message}"
+        return restTemplate.getForObject("http://nacos-discovery-provider-sample/echo/" + message, String.class);
+    }
+}
